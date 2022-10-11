fe65dcdcc8cb17349a3b3bc9eb864f60b9ad1c5f
Petar Angelov Kostadinov <petar.kostadinov@gameloft.com>
2022-10-11 14:58:50 +0300

    Added functionality to Library

--- ServerLibraryComponent.cpp
+++ ServerLibraryComponent.cpp
@@ -16,3 +16,66 @@ const int ServerLibraryComponent::GetIndexOfBook(const book& toSearch)
     }
     return -1;
 }
+
+const int ServerLibraryComponent::GetIndexOfUser(const user& toSearch)
+{
+    auto size = users.size();
+    for (size_t i = 0; i < size; i++)
+    {
+        if (users[i] == toSearch)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void ServerLibraryComponent::AddUser(const user toAdd)
+{
+    if (GetIndexOfUser(toAdd) != -1)
+    {
+        users.push_back(toAdd);
+    }
+}
+
+void ServerLibraryComponent::AddBook(const book toAdd)
+{
+    if (GetIndexOfBook(toAdd) != -1)
+    {
+        books.push_back(toAdd);
+    }
+}
+
+bool ServerLibraryComponent::RemoveUser(const user&& toRemove)
+{
+    bool status = false;
+    std::vector <user> newVector;
+    for (auto temp : users)
+    {
+        if (temp == toRemove)
+        {
+            status = true;
+            continue;
+        }
+        newVector.push_back(temp);
+    }
+    users = newVector;
+    return status;
+}
+
+bool ServerLibraryComponent::RemoveBook(const book&& toRemove)
+{
+    bool status = false;
+    std::vector <book> newVector;
+    for (auto temp : books)
+    {
+        if (temp == toRemove)
+        {
+            status = true;
+            continue;
+        }
+        newVector.push_back(temp);
+    }
+    books = newVector;
+    return status;
+}
